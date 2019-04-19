d4d4d01274752fb48970614a226b68f9801aece0
Andrii Dederia <derya_86@ukr.net>
2019-04-19 12:46:12 +0300

    port 80

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const dbPassword = "%40e8srxwkp";
 
 const config = {
   secret: "secret-301020",
-  port: 8080,
+  port: 80,
   dbUser,
   dbPassword,
   databaseUrl: `mongodb+srv://derya:${dbPassword}@cluster0-e2bim.mongodb.net/foody?retryWrites=true`
